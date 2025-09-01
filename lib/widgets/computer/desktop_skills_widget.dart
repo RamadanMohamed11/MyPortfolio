2f757965fc7a60ebe345692824ee841e6d005348
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-09-01 04:52:09 +0300

    make simple animation on skills container to zoom in when mouse move across it

--- lib/widgets/computer/desktop_skills_widget.dart
+++ lib/widgets/computer/desktop_skills_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_portfolio/constants/colors.dart';
 import 'package:my_portfolio/constants/skill_items.dart';
 
-class DesktopSkillsWidget extends StatelessWidget {
+class DesktopSkillsWidget extends StatefulWidget {
   const DesktopSkillsWidget({
     super.key,
     required this.navBarKeys,
@@ -14,11 +14,18 @@ class DesktopSkillsWidget extends StatelessWidget {
   final Size screenSize;
 
   @override
+  State<DesktopSkillsWidget> createState() => _DesktopSkillsWidgetState();
+}
+
+class _DesktopSkillsWidgetState extends State<DesktopSkillsWidget> {
+  final List<double> _scale = List.generate(12, (_) => 1.0);
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 10.w),
       padding: EdgeInsets.symmetric(vertical: 5.h),
-      key: navBarKeys[3],
+      key: widget.navBarKeys[3],
       width: double.infinity,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(15),
@@ -36,32 +43,40 @@ class DesktopSkillsWidget extends StatelessWidget {
             children: [
               ConstrainedBox(
                 constraints: BoxConstraints(
-                  maxWidth: screenSize.width,
+                  maxWidth: widget.screenSize.width,
                 ),
                 child: Wrap(
                   children: [
                     for (int i = 0; i < mySkills.length; i++)
-                      Container(
-                        margin: const EdgeInsets.all(15),
-                        width: 115.w,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(45),
-                            color: CustomColor.bgLighter2),
-                        child: Center(
-                          child: ListTile(
-                            isThreeLine: false,
-                            leading: Image.asset(
-                              mySkills[i]["img"],
-                              //fit: BoxFit.cover,
-                              width: 25.w,
-                              height: 40.h,
-                            ),
-                            title: FittedBox(
-                              alignment: Alignment.centerLeft,
-                              fit: BoxFit.scaleDown,
-                              child: Text(
-                                mySkills[i]["title"],
-                                style: TextStyle(fontSize: 5.7.sp),
+                      AnimatedScale(
+                        duration: const Duration(milliseconds: 200),
+                        scale: _scale[i],
+                        child: MouseRegion(
+                          onEnter: (_) => setState(() => _scale[i] = 1.1),
+                          onExit: (_) => setState(() => _scale[i] = 1.0),
+                          child: Container(
+                            margin: const EdgeInsets.all(15),
+                            width: 115.w,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(45),
+                                color: CustomColor.bgLighter2),
+                            child: Center(
+                              child: ListTile(
+                                isThreeLine: false,
+                                leading: Image.asset(
+                                  mySkills[i]["img"],
+                                  //fit: BoxFit.cover,
+                                  width: 25.w,
+                                  height: 40.h,
+                                ),
+                                title: FittedBox(
+                                  alignment: Alignment.centerLeft,
+                                  fit: BoxFit.scaleDown,
+                                  child: Text(
+                                    mySkills[i]["title"],
+                                    style: TextStyle(fontSize: 5.7.sp),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
